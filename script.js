c0e214b690f69c7fb7867ee1ed9b1cb929b6a20b
whozsk <vexedgunz@gmail.com>
2025-08-20 14:02:01 -0700

    i2 song.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function playsong() {
         audio.pause();
         audio.currentTime = 0;
     } else {
-        audio = new Audio('hot (mandy).mp3');
+        audio = new Audio('i2.mp3');
     }
     
     audio.play().catch(e => console.error('Audio playback failed:', e));
